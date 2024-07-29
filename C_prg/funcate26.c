2f99cbe0803e1a9d8120029b136d818048c62d2f
Anjana-lad <iantanjanalad@gmail.com>
2024-07-29 16:53:01 +0530

    add functions

--- C_prg/funcate26.c
+++ C_prg/funcate26.c
@@ -6,7 +6,7 @@
 
 // #include<stdio.h>
 // void cube(){
-//     int side,res;
+//     int side,res;  //local parameter
 //     printf("Enter side;");
 //     scanf("%d",&side);
 //     res=side*side*side;
@@ -17,13 +17,38 @@
 // }
 
 // no argument with return 
+// #include<stdio.h>
+// int sum(){
+//     int a,b;
+//     printf("Enter number:");
+//     scanf("%d %d",&a,&b);
+//     return a+b;
+// }
+// int main(){
+//     printf("Sum of (a+b)=%d",sum());
+// }
+
+//with arguments no return
+// #include<stdio.h>
+// int sub_fun(int a,int b){
+//    int res=a-b;
+//    printf("Result=%d",res);
+// }
+// int main(){
+//     int a,b;  //local parameter
+//     printf("Enter numbers:");
+//     scanf("%d %d",&a,&b);
+//     sub_fun(a,b);
+// }
+
+// with argument with return
 #include<stdio.h>
-int sum(){
-    int a,b;
-    printf("Enter number:");
-    scanf("%d %d",&a,&b);
-    return a+b;
+int add_fun(int num1,int num2){   //formal parameter
+    return num1+num2;
 }
 int main(){
-    printf("Sum of (a+b)=%d",sum());
+    int a,b;
+    printf("Enter numbers:");
+    scanf("%d %d",&a,&b);
+    printf("calling function add_fun=%d",add_fun(a,b));  //actual parameter
 }
